3a42589f4cad2f55de3bced0e1ebe9e96971a660
Guilherme Tarifa Vieira <guilherme.vieira@sptech.school>
2023-05-06 11:27:42 -0300

    imagem res com innerWidth

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Img from "../html-css-template/imagens/sound-waves-pink.png";
 import { useNavigate } from "react-router-dom";
 
 function Home() {
   const navigate = useNavigate();
+
+  const [image, setImage] = useState(true);
+
+  useEffect(() => {
+    function handleResize() {
+      if (window.innerWidth <= 700) {
+        setImage(false);
+        console.log(image);
+      } else if (window.innerWidth > 700) {
+        setImage(true);
+        console.log(image);
+      }
+    }
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, [image]);
 
   return (
     <>
@@ -11,20 +29,28 @@ function Home() {
         <div className="index-content">
           <div className="box-text-index">
             <div className="info-index">
-              <blockquote className="my-custom-blockquote">
-                <p className="display">A música certa para o seu mood</p>
+              <blockquote
+                className={`my-custom-blockquote ${!image ? "custom-min" : ""}`}
+              >
+                <p className={`display ${!image ? "display-min" : ""}`}>
+                  A música certa para o seu mood
+                </p>
                 <p className="subtitle">
                   organize suas músicas preferidas por categorias e aproveite
                   cada batida{" "}
                 </p>
               </blockquote>
             </div>
-            <div className="img-index"></div>
             <button className="btn" onClick={() => navigate("/musicas")}>
               começar
             </button>
           </div>
-          <img src={Img} alt="" className="index-img" />
+          <img
+            src={Img}
+            alt=""
+            className={`index-img ${!image ? "img-false" : ""}`}
+            s
+          />
         </div>
       </div>
     </>
